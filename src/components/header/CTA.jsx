5e1226665c480f4a06db1b834ff2bb14b2bb5336
divyasrinaraharisetti <naraharisettidivyasri@gmail.com>
2024-02-01 17:15:01 -0500

    resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,9 +4,10 @@ import React from "react";
 const CTA = () => {
   const handleDownloadClick = () => {
     // Replace the URL with the actual link to your PDF
-    const cvUrl = "https://uflorida-my.sharepoint.com/:b:/g/personal/d_naraharisetti_ufl_edu/EQ1G8kUk6ipIs8vWZYAt6xoBc8nW3RR-sxU6ytv5g-efiQ?e=7mn5v5";
+    const cvUrl = "https://uflorida-my.sharepoint.com/:b:/g/personal/d_naraharisetti_ufl_edu/EbLOmlOaJzZAvj4mzimK0vUBZ8MmhmU9hjgr2Akcbeuh6A?e=R4snmm";
     //https://drive.google.com/file/d/1DlS2-X8CNhMS8AmxaICkL-Ujl_6ufj5-/view
     // https://uflorida-my.sharepoint.com/:b:/g/personal/d_naraharisetti_ufl_edu/EQ1G8kUk6ipIs8vWZYAt6xoBc8nW3RR-sxU6ytv5g-efiQ?e=7mn5v5
+    // https://uflorida-my.sharepoint.com/:b:/g/personal/d_naraharisetti_ufl_edu/EbLOmlOaJzZAvj4mzimK0vUBZ8MmhmU9hjgr2Akcbeuh6A?e=R4snmm
     // Open the CV link in a new tab
     window.open(cvUrl, '_blank');
   };
